afebe51ce1993bf795c3386d8906ea36fd660735
U-LB\chanwit <chanwit@gmail.com>
2009-02-15 04:00:47 +0700

    refactored getAst()

---
+++ src/org/codehaus/groovy/typesheet/parser/Main.java
@@ -0,0 +1,41 @@
+package org.codehaus.groovy.typesheet.parser;
+
+import org.antlr.runtime.tree.*;
+import org.antlr.runtime.*;
+
+public class Main {
+
+static final TreeAdaptor adaptor = new CommonTreeAdaptor() {
+	public Object create(Token payload) {
+		return new CommonTree(payload);
+	}
+};
+
+    public static CommonTree getAst(String filename) {
+        ANTLRFileStream fs = new ANTLRFileStream(filename);
+		TypesheetLexer lex = new TypesheetLexer(fs);
+		TokenRewriteStream tokens = new TokenRewriteStream(lex);
+		TypesheetParser grammar = new TypesheetParser(tokens);
+		grammar.setTreeAdaptor(adaptor);
+		TypesheetParser.compilationUnit_return ret = grammar.compilationUnit();
+		CommonTree tree = (CommonTree)ret.getTree();        
+		return tree;
+    }
+
+	public static void main(String[] args) throws Throwable {
+		printTree(getAst(args[0]), 0);
+	}
+	
+	public static void printTree(CommonTree t, int indent) {
+		if ( t != null ) {
+			StringBuffer sb = new StringBuffer(indent);
+			for ( int i = 0; i < indent; i++ )
+				sb = sb.append("   ");
+			for ( int i = 0; i < t.getChildCount(); i++ ) {
+				System.out.println(sb.toString() + t.getChild(i).toString());
+				printTree((CommonTree)t.getChild(i), indent+1);
+			}
+		}
+	}
+	
+}
